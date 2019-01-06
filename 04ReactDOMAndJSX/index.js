94a8016274480dc0873a3c4932e013c96839d0cc
Kris <christttto@berkeley.edu>
2019-01-05 22:05:17 -0800

    learn react

---
+++ 04ReactDOMAndJSX/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// JSX sudo html taf for us, must have one element, that can wrap around several elements
+ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
