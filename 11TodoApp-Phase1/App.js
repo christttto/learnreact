ea5b394cdd89ca44ded000804964df56f3d20946
Kris <christttto@berkeley.edu>
2019-01-06 00:16:03 -0800

    11 first project done

---
+++ 11TodoApp-Phase1/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function App(){
+    return(
+        <div>
+            <input type="checkbox" />
+            <p>react website</p>
+            <input type="checkbox" />
+            <p>life</p>
+            <input type="checkbox" />
+            <p>sponsorship</p>
+        </div>
+    )
+}
+
+export default App
